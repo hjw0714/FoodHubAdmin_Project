db5ec6673a07d3a25293051f28078f60f70dc1f8
kwonsb96 <kwonsb96@gmail.com>
2025-04-25 17:44:44 +0900

    관리자 게시글 작성

---
+++ foodhubAdmin_backend/src/main/java/com/application/foodhubAdmin/domain/FileUpload.java
@@ -0,0 +1,4 @@
+package com.application.foodhubAdmin.domain;
+
+public class FileUpload {
+}
---
+++ foodhubAdmin_backend/src/main/java/com/application/foodhubAdmin/repository/FileUploadRepository.java
@@ -0,0 +1,4 @@
+package com.application.foodhubAdmin.repository;
+
+public interface FileUploadRepository {
+}
